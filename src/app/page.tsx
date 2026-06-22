8422df53ac944cfbedc1087e469c9bbfda6a0f5e
Sahla1138 <sahla.sahanas.raihsoft@gmail.com>
2026-06-22 12:21:28 +0530

    Update portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,7 +60,7 @@ const experiences = [
   <div className="absolute inset-0 bg-cyan-500 rounded-full blur-3xl opacity-30 animate-pulse"></div>
 
 <Image
-  src="/profile.webp"
+  src="/profile.jpeg"
   alt="Profile"
   width={224}
   height={224}
